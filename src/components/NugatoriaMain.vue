a9ba68044ea417418ff56b3e878bb7ddd9f9821b
jimmer <jimmr44@gmail.com>
2023-05-08 05:51:45 -0400

    New updates

---
+++ src/components/NugatoriaMain.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="nugatoriamain">
+        
+    <h1>{{ msg }}</h1>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'NugatoriaMain',
+  props: {
+    msg: String
+  }
+}
+
+</script>
