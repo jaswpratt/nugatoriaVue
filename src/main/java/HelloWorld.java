ba326166db3d15e81a4c5c74743cab21c43fb769
Jas <55251065+jaswpratt@users.noreply.github.com>
2023-05-18 13:03:25 -0400

    Update HelloWorld.java
    New Experimental code.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,1 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.InputStreamReader;
 
+public class MyClass {
+
+  public static void main(String[] args) {
+      Scanner scanner = new Scanner(new InputStreamReader(System.in));
+    String originalStr = "Hello";
+    String reversedStr = "";
+    System.out.println("Original string: " + originalStr);
+        
+    for (int i = 0; i < originalStr.length(); i++) {
+      reversedStr = originalStr.charAt(i) + reversedStr;
+      System.out.println("reversedStr ===> " + reversedStr);
+//      System.out.println("Please hit enter: ");
+    //    String input = scanner.nextLine();
+    }
+    
+    System.out.println("Reversed string: "+ reversedStr);
+  }
+
+}
